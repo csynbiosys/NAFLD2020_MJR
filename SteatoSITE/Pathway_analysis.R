9f37f462e161b5b790a795bf00e87a591deeb202
mariajimramos <77021659+mariajimramos@users.noreply.github.com>
2021-05-05 11:20:06 +0200

    Function for pathway analysis (with arbitrary threshold). Function for differential expression analysis. Function for MA and volcano plot.

---
+++ SteatoSITE/Pathway_analysis.R
@@ -0,0 +1,54 @@
+Pathway_analysis <- function(dir, type, fc_threshold){
+  file <- read.csv(dir, row.names = 1)
+  file <- subset(file, (file$FDR < 0.05))
+  file <- subset(file, (file$logFC > fc_threshold|file$logFC < fc_threshold))
+  file <- subset(file, file$entrez!= "<NA>")
+  file <- file[which(duplicated(file$entrez) == FALSE), ]
+  geneList <- file$logFC
+  names(geneList) <- as.character(file$entrez)
+  geneList <- sort(geneList, decreasing =TRUE)
+  
+  reactome <- read.gmt("./c2.cp.reactome.v7.2.entrez.gmt")  #gmt file that contains the reactome sets (downloaded from Braod Institute)
+  reactome_enrich <- enricher(names(geneList), TERM2GENE= reactome)
+  reactome_enrich_results <- reactome_enrich@result
+  if (reactome_enrich@result$p.adjust[1] >= 0.05){
+    print("No enrich reactome pathway with p.adjust value < 0.05!")
+  } else {
+    write.csv(reactome_enrich_results, paste("./Pathways/Reactome", type, "enrich.csv", sep = "_"))
+    pdf(paste("./Pathways/Barplot_reactome", type, "enrich.pdf", sep = "_"), width = 15, height = 12)
+    p <- barplot(reactome_enrich)
+    print(p)
+    dev.off()
+    pdf(paste("./Pathways/Network_reactome", type, "enrich.pdf"), width = 30, height = 20)
+    #network <- setReadable(reactome_enrich, org.Hs.eg.db, "ENTREZID")  #This function transforms the entrez id into symbols for the networks. 
+    #p <- cnetplot(network, foldChange = geneList, showCategory = 4)  #It creates a network with the top 4 pathways obtained and the genes that have been found from that pathway in the DEG list. Those genes will have different colour depending on the fold-change. 
+    #print(p)
+    dev.off()
+  }
+  
+  go <- read.gmt("./c5.go.v7.3.entrez.gmt")  #gmt file that contains GO terms (CC, BP and MF).
+  go_enrich <- enricher(names(geneList), TERM2GENE = go, pvalueCutoff = 0.05, qvalueCutoff = 0.05)
+  go_enrich_results <- go_enrich@result
+  if (go_enrich@result$p.adjust[1] >= 0.05){
+    print("No enrich GO term with p.adjust value < 0.05!")
+  } else {
+    write.csv(go_enrich_results, paste("./Pathways/GO", type, "enrich.csv", sep = "_"))
+    pdf(paste("./Pathways/Dotplot", type, "go_enrich.pdf", sep = "_"), width = 15, height = 14)
+    p <- dotplot(go_enrich, font.size = 8)  #It creates a dotplot with the top 10 GO terms linked with the DEG list. Each dot will have a different colour, depending on their p.adjust value, and a different size, depending on the number of counts on each GO term.
+    print(p)
+    dev.off()
+  }
+  
+  kegg <- read.gmt("./c2.cp.kegg.v7.2.entrez.gmt")  #gmt file that contains the KEGG pathways (downloaded from Broad Institute)
+  kegg_enrich <- enricher(names(geneList), TERM2GENE = kegg, pvalueCutoff = 0.05, qvalueCutoff = 0.05)
+  kegg_enrich_results <- kegg_enrich@result
+  if (kegg_enrich@result$p.adjust[1] >= 0.05){
+    print("No enrich KEGG pathway with p.adjust value < 0.05!")
+  } else {
+    write.csv(kegg_enrich_results, paste("./Pathways/Kegg", type, "enrich.csv", sep = "_"))
+    pdf(paste("./Pathways/Dotplot", type, "kegg_enrich.pdf", sep = "_"), width = 15, height = 12)
+    p <- dotplot(kegg_enrich)
+    print(p)
+    dev.off()
+  }
+}
---
+++ SteatoSITE/ma_volcano_plot.R
@@ -0,0 +1,23 @@
+ma_plot<- function(results_table, fc_threshold, fdr_threshold){
+  results_table$significant <- 'no'
+  results_table$significant[ results_table$FDR <= fdr_threshold ] <- 'yes'
+  ggplot(results_table, aes(logCPM, logFC, color=significant )) + 
+    geom_point(size = 0.8) + # This alters the transparency of the points 
+    scale_colour_manual(name = 'significant', # provides the label
+                        values = setNames(c('red','grey'),c('yes', 'no'))) +
+    geom_hline(yintercept=log2(fc_threshold), linetype= "dashed") +
+    geom_hline(yintercept=-1*log2(fc_threshold), linetype= "dashed") + 
+    theme_bw()
+}
+
+volcano_plot<- function(results_table, fc_threshold=2, fdr_threshold=0.05,label_threshold=9){
+  genes<-results_table
+  genes$Significant<-ifelse((genes$FDR < fdr_threshold & (genes$logFC>=log2(fc_threshold)|genes$logFC<=log2(fc_threshold)*-1)), 'Yes', 'No') #adds a column in the `genes` table to show whether each gene is significantly expressed
+  ggplot(data=genes, aes(logFC, -log10(FDR))) + # the values for the x and y axis
+    geom_point(aes(col=Significant),size=0.8)+# shows how the points will look
+    geom_text_repel(aes(label=ifelse(abs(logFC)>=label_threshold,as.character(row.names(genes)),''))) + # labels all genes with a log fold change greater than the specified threshold
+    scale_colour_manual(name = 'Significant', values =
+                          setNames(c('red','grey'),c('Yes', 'No'))) +# shows how to decide the colour
+    geom_vline(xintercept=c(log2(fc_threshold),log2(fc_threshold)*-1), linetype = "dashed") + # adds the vertical indicating our chosen fold change threshold
+    theme_bw()
+}
